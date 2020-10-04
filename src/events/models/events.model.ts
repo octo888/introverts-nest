fb28da312f3865f95f8dbec1b611bb2a727e7ff1
Viktor Moroz <moroz@newtonideas.com>
2020-10-04 15:48:54 +0300

    event model

--- src/events/models/events.model.ts
+++ src/events/models/events.model.ts
@@ -1,8 +1,20 @@
 export interface ScheduleEvent {
-  id: string
+  id: string;
+  createdDate: string;
+  scheduledDate: string;
+  startTime: string;
+  endTime: string;
+  location: string;
+  description: string;
+  isRegistrationRequired: boolean;
+  mainImg: string;
+  images: string[];
+  registeredEmails: string[];
+  status: ScheduleEventStatus
 }
 
-export enum EventStatus {
-  OPEN = 'OPEN',
-  IN_PROGRESS = 'IN_PROGRESS'
+export enum ScheduleEventStatus {
+  UPCOMING = 'UPCOMING',
+  IN_PROGRESS = 'IN_PROGRESS',
+  COMPLETED = 'COMPLETED'
 }
